c813f2d847ded84542654ea0a094400131fb8dff
soyacen <soyacen@outlook.com>
2025-07-10 15:01:20 +0800

    feat: add proto file

--- example/compile.sh
+++ example/compile.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 protoc \
 --proto_path=. \
 --proto_path=../third_party \
 --proto_path=../../ \
 --go_out=. \
 --go_opt=paths=source_relative \
---gors_out=. \
---gors_opt=paths=source_relative \
+--gorilla_out=. \
+--gorilla_opt=paths=source_relative \
 */*.proto
