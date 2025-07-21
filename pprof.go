f365a3832aa3d22d00cf3f3b25baf7d44196f611
soyacen <soyacen@outlook.com>
2025-07-21 18:50:30 +0800

    feat: add AppendPProf route

---
+++ pprof.go
@@ -0,0 +1,16 @@
+package gorilla
+
+import (
+	"net/http/pprof"
+
+	"github.com/gorilla/mux"
+)
+
+func AppendPProf(router *mux.Router) *mux.Router {
+	router.PathPrefix("/debug/pprof/").HandlerFunc(pprof.Index)
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	return router
+}
