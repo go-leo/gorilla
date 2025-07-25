b120a40ad2f0197ed6064ce6c65d23a66e315eaf
soyacen <soyacen@outlook.com>
2025-07-25 17:48:19 +0800

    feat: support grpc timeout units

--- middleware/timeout/middleware.go
+++ middleware/timeout/middleware.go
@@ -3,19 +3,54 @@ package timeout
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/exp/slog"
 )
+
+const key = "X-Leo-Timeout"
 
 func Middleware(duration time.Duration) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			timeout := duration
+			value := r.Header.Get(key)
+			if value != "" {
+				switch {
+				case strings.HasSuffix(value, "n"):
+					value = value + "s"
+				case strings.HasSuffix(value, "u"):
+					value = value + "s"
+				case strings.HasSuffix(value, "m"):
+					value = value + "s"
+				case strings.HasSuffix(value, "S"):
+					value = strings.Replace(value, "S", "s", 1)
+				case strings.HasSuffix(value, "M"):
+					value = strings.Replace(value, "M", "m", 1)
+				case strings.HasSuffix(value, "H"):
+					value = strings.Replace(value, "H", "h", 1)
+				}
+				incomingDuration, err := time.ParseDuration(value)
+				if err != nil {
+					slog.Error("timeout parse error", slog.String("timeout", value), slog.String("error", err.Error()))
+				} else {
+					timeout = min(incomingDuration, duration)
+				}
+			}
 			ctx := r.Context()
-			ctx, cancel := context.WithTimeout(ctx, duration)
+			ctx, cancel := context.WithTimeout(ctx, timeout)
 			defer cancel()
 			r = r.WithContext(ctx)
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func min(a, b time.Duration) time.Duration {
+	if a < b {
+		return a
+	}
+	return b
 }
